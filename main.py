f91ecfa1e624ff1f29474a63a4d721bd0f767a7b
joeythornberry <lewis.thornberry@christendom.edu>
2023-06-22 12:23:26 -0400

    coerces window size before playing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import navigation
 import fight_seasonal_battles
 import locations
+import time
 
+locations.coerce_window_size()
+time.sleep(3)
 locations.load_location_pixels()
 navigation.get_to_seasonal_screen()
 fight_seasonal_battles.fight_seasonal_battles()
