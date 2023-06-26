765351ca35c2b2b4c75731b5d11f51f145d593ff
joeythornberry <lewis.thornberry@christendom.edu>
2023-06-26 13:09:56 -0400

    add test for emote function

---
+++ tests/emote_test.py
@@ -0,0 +1,6 @@
+from shared import locations
+from battle import fight_seasonal_battles
+
+print("to run this: run [python -m tests.emote_test] from a command line in the project-one-year-old-master directory")
+locations.load_location_pixels()
+fight_seasonal_battles.emote()
