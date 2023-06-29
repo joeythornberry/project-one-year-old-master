7599b37845dd6861a588ecbe1c620871108ca905
joeythornberry <lewis.thornberry@christendom.edu>
2023-06-29 14:19:29 -0400

    clan battle until 4 battles fought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from battle import battle_navigation
+from battle import fight_clan_battles
 from battle import fight_seasonal_battles
 from shared import locations
 import time
@@ -6,6 +7,6 @@ import time
 locations.coerce_window_size()
 time.sleep(3)
 locations.load_location_pixels()
-battle_navigation.get_to_seasonal_screen()
-fight_seasonal_battles.fight_seasonal_battles()
+battle_navigation.get_to_clan_screen()
+fight_clan_battles.fight_clan_battles()
 battle_navigation.get_to_home_screen()
